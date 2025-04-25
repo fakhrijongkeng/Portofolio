cc8d6e1629385bf820d6916c44154a95c1578d44
fakhrijongkeng <fakhrijongkeng12@gmail.com>
2025-04-25 19:02:25 +0700

    Update nsv.py

--- Page/nsv.py
+++ Page/nsv.py
@@ -1,16 +1,4 @@
 import streamlit as st
 
-# Set up the navigation
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Projects", "Contact"])
-
-# Show the selected page
-if page == "Home":
-    st.title("Welcome to my portfolio!")
-    st.write("This is the home page.")
-elif page == "Projects":
-    st.title("My Projects")
-    st.write("Here are some of my Python/Data Science projects.")
-elif page == "Contact":
-    st.title("Contact Me")
-    st.write("Email: you@example.com")
+st.title("Projects")
+st.write("Here are some of my Python and Data Science projects.")
