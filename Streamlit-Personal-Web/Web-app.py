bc7a55eebcfbd8c1299a1dfbdd0c388859cf5df5
fakhrijongkeng <fakhrijongkeng12@gmail.com>
2025-05-06 11:39:07 +0700

    Update Web-app.py

--- Streamlit-Personal-Web/Web-app.py
+++ Streamlit-Personal-Web/Web-app.py
@@ -5,7 +5,7 @@ col1, col2 = st.columns([1, 3])
 st.title("FAKHRI AKMAL")
 
 with col1:
-    st.image(""https://raw.githubusercontent.com/fakhrijongkeng/Portofolio/main/Streamlit-Personal-Web/images/WhatsApp%20Image%202024-10-07%20at%2019.27.27_6ab775b0.jpg"")
+    st.image("https://raw.githubusercontent.com/fakhrijongkeng/Portofolio/main/Streamlit-Personal-Web/images/WhatsApp%20Image%202024-10-07%20at%2019.27.27_6ab775b0.jpg")
     
 with col2:
     st.write("This is my personal web app")
