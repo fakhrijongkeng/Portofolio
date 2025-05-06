9380eeb6ea2cfa939d41e74f3a0a45cec9e9ad24
fakhrijongkeng <fakhrijongkeng12@gmail.com>
2025-05-06 11:04:54 +0700

    Update Web-app.py

--- Streamlit-Personal-Web/Web-app.py
+++ Streamlit-Personal-Web/Web-app.py
@@ -1,22 +1,14 @@
 import streamlit as st
 
-col1, col2, col3 = st.columns([1, 2, 1])
+col1, col2 = st.columns([1, 3])
+ 
+st.title("FAKHRI AKMAL")
 
 with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
+    st.image("sunrise.jpg", caption="Sunrise by the mountains")
+    
 with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("Another cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-
-st.title("FAKHRI AKMAL")
-st.write("This is my personal web app")
+    st.write("This is my personal web app")
 
 st.page_link("Web-app.py", label="Home")
 st.page_link("pages/portofolio.py", label="Page 1", icon="1️⃣")
