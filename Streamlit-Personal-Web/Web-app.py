1b2c31204e8df6685c7cbd10cf0300302612ca4d
fakhrijongkeng <fakhrijongkeng12@gmail.com>
2025-05-06 10:47:12 +0700

    Update Web-app.py

--- Streamlit-Personal-Web/Web-app.py
+++ Streamlit-Personal-Web/Web-app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("FAKHRI AKMAL")
 st.write("This is my personal web app")
 
-st.page_link("Web-app.py", label="Home", icon="🏠")
+st.page_link("Web-app.py", label="Home")
 st.page_link("pages/portofolio.py", label="Page 1", icon="1️⃣")
